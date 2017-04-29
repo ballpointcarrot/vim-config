9b757267e42f05955134c3ba250b375d2457aa74
Christopher Kruse <krusec@amazon.com>
2017-04-29 15:45:37 -0700

    added installer for vim-plug.

---
+++ install_plug.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+echo "Installing vim-plug for both vim and neovim..."
+
+URL="https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim"
+
+# For standard VIM
+curl -fLo ~/.vim/autoload/plug.vim --create-dirs $URL
+
+# For Neovim
+curl -fLo ~/.local/share/nvim/site/autoload/plug.vim --create-dirs $URL
+
+echo "Installation complete. Run \":PlugInstall\" on first run."
